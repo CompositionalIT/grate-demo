4ec6c4050174fdbaa50d0e23bcff62e2c424f384
Matt Gallagher <matt@compositional-it.com>
2022-09-30 15:43:08 +0100

    Add script to create SettlementType table

---
+++ up/0001_SettlementType.sql
@@ -0,0 +1,11 @@
+CREATE TABLE SettlementType (
+    ID INT PRIMARY KEY,
+    Name NVARCHAR(10)
+)
+
+INSERT INTO SettlementType
+VALUES
+    (1, 'Hamlet'),
+    (2, 'Village'),
+    (3, 'Town'),
+    (4, 'City')
